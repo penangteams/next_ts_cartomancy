429420c2874d250afad5117240fe17fd477d513f
Penang Teams <raj_ppd@yahoo.com>
2024-10-18 02:01:00 +0800

    changed nav bg colors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,11 +7,11 @@ import { Searchbox } from "./Searchbox";
 const Navbar = () => {
   return (
     <div className="sticks">
-      <nav className="bg-violet-600 flex pt-2 pl-2 py-3 mb-3">
+      <nav className="bg-[#3863A0] flex pt-2 pl-2 py-3 mb-3">
         <ModeToggle />
         <Searchbox />
         <div className="ml-auto mr-3 ">
-          <button className="bg-yellow-400 text-black rounded px-3 py-2">
+          <button className="bg-[#1E3250] text-white rounded px-3 py-2">
             Action
           </button>
         </div>
